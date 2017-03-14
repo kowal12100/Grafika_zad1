cf475feb4232a31a387ad4041878be73012e3c3c
Kamil Kowalczyk <kamil12100@gmail.com>
2017-03-14 20:40:24 +0100

    Add files via upload

---
+++ 1/Pierwsze Sprawozdanie/Pierwsze Sprawozdanie.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+void RenderScene(void) {
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glFlush();
+}
+
+void SetupRC(void) {
+	glClearColor(0.58f, 0.29f, 0.0f, 1.0f);
+}
+
+void main(int argc, char **argv) {
+
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutCreateWindow("Moj pierwszy program w GLUT");
+	glutDisplayFunc(RenderScene);
+	SetupRC();
+	glutMainLoop();
+}
